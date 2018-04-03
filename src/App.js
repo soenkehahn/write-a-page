c8eff1e7d75905d09dd34073407a5310b885c5ce
Sönke Hahn <soenkehahn@gmail.com>
2018-04-03 17:34:54 -0400

    allow to enter words

--- src/App.js
+++ src/App.js
@@ -2,8 +2,23 @@
 
 import React, { Component } from "react";
 
-export class App extends Component<*> {
+export class App extends Component<{}, { value: string }> {
+  constructor(props: {}) {
+    super(props);
+    this.state = { value: "" };
+  }
+
+  handleChange(event: SyntheticInputEvent<HTMLInputElement>) {
+    this.setState({ value: event.target.value });
+  }
+
   render() {
-    return <textarea autoFocus={true} />;
+    return (
+      <textarea
+        autoFocus={true}
+        value={this.state.value}
+        onChange={event => this.handleChange(event)}
+      />
+    );
   }
 }
--- src/App.test.js
+++ src/App.test.js
@@ -5,14 +5,29 @@ import { mount } from "enzyme";
 import { App } from "./App";
 
 describe("App", () => {
+  let wrapper;
+  let textarea;
+
+  beforeEach(() => {
+    wrapper = mount(<App />);
+    textarea = wrapper.find("textarea");
+  });
+
+  afterEach(() => {
+    wrapper.unmount();
+  });
+
   it("displays a text area tag", () => {
-    const wrapper = mount(<App />);
-    expect(wrapper.find("textarea")).toExist();
+    expect(textarea).toExist();
   });
 
   it("focuses on the text area", () => {
-    const wrapper = mount(<App />);
-    expect(wrapper.find("textarea").props().autoFocus).toBe(true);
+    expect(textarea.props().autoFocus).toBe(true);
+  });
+
+  it("allows to enter text", () => {
+    textarea.simulate("change", { target: { value: "foo bar baz" } });
+    expect(textarea).toExist();
   });
 
   describe("when over 300 words are written", () => {
